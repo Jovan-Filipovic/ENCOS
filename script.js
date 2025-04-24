69f0d03d03eeff528f7f1e5043d45e6a3c7bbf35
Jovan-Filipovic <117949587+Jovan-Filipovic@users.noreply.github.com>
2025-04-24 16:20:12 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  function updateDateTime() {
+    const dateTimeElement = document.querySelector('.date-time');
+    const now = new Date();
+    dateTimeElement.textContent = now.toLocaleString();
+  }
+    setInterval(updateDateTime, 1000); // Update the date and time every second
+  </script>
